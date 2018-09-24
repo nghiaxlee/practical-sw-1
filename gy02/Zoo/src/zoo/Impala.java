1e2bb8ffd7703362911270d258b850ca2a7867bd
Le Minh Nghia <aaogmu@INF.ELTE.HU>
2018-09-24 09:39:10 +0200

    Practice 2

---
+++ gy02/Zoo/src/zoo/Impala.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+/**
+ *
+ * @author aaogmu
+ */
+public class Impala extends WildAnimal {
+
+    @Override
+    public void move() {
+        super.move(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Impala roar");
+    }
+    
+    
+    
+}
